e15a9a799cafc956bc7e2a13e06c375d55ff5ee6
Oleksandr_Hliebov <Oleksandr_Hliebov@epam.com>
2017-03-27 19:38:08 +0300

    homework #4: onpush strategy

--- src/app/core/course/course.component.ts
+++ src/app/core/course/course.component.ts
@@ -1,14 +1,37 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  NgZone } from '@angular/core';
 import { Course } from './course.type';
+
+let t1 = 0;
 
 @Component({
   selector: 'course',
   templateUrl: 'course.template.html',
-  styleUrls: ['course.style.scss']
+  styleUrls: ['course.style.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 class CourseComponent {
   @Input() public course: Course;
   @Output() public courseOutput: EventEmitter<Course> = new EventEmitter();
+
+  constructor(private ngZone: NgZone) {
+    this.ngZone.onStable.subscribe(this.onZoneStable);
+    this.ngZone.onUnstable.subscribe(this.onZoneUnstable);
+  }
+
+  public onZoneStable() {
+    let timeDiff = +new Date() - t1;
+    console.log('We are stable, execution time: ', timeDiff);
+  }
+
+  public onZoneUnstable() {
+    t1 = +new Date();
+    console.log('We are unstable');
+  }
 
   public getProperDate(date: number): string {
     return (new Date(date)).toDateString();
