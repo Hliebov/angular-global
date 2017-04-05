47fad64e6497b19f4789fdb7f1e196d26763cab2
Oleksandr_Hliebov <Oleksandr_Hliebov@epam.com>
2017-04-05 13:52:34 +0300

    homework #5: filterPipe Course type

--- src/app/shared/filterPipe/filterPipe.ts
+++ src/app/shared/filterPipe/filterPipe.ts
@@ -1,8 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Course } from './../../core/course/course.type';
 
 @Pipe({name: 'myFilterPipe'})
 class FilterPipe implements PipeTransform {
-  public transform<T>(entities: T[], filterBy: string): T[] {
+  public transform(entities: Course[], filterBy: string): Course[] {
     return entities.filter((entity) => {
       return entity.title.indexOf(filterBy) > -1;
     });
